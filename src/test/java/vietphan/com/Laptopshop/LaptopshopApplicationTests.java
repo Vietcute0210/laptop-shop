ea0792f5c6795c2788333c337be606305b244d9d
Vietdudu0210 <vietqhoa0210@gmail.com>
2026-01-22 23:28:42 +0700

    Initial commit

---
+++ src/test/java/vietphan/com/Laptopshop/LaptopshopApplicationTests.java
@@ -0,0 +1,13 @@
+package vietphan.com.Laptopshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaptopshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
